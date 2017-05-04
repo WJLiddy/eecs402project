73a7e80d79ce1b0d7ee888f8c78f7942cadddeee
WJLIDDY <wjl36@case.edu>
2017-05-04 14:17:09 -0400

    update stats

--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@
 # Get all files in results/
 from os import listdir
 from os.path import isfile, join
+import os.path as path
 
 # numpy is for losers lmao
 def avg(lst):
@@ -23,31 +24,27 @@ def pstdev(data):
 	return pvar**0.5
 
 
-onlyfiles = [f for f in listdir("results/") if isfile(join("results/", f))]
-
-avgs = []
+result_folders = [f for f in listdir("results/") if path.isdir(join("results/", f))]
 
 
+for result_folder in result_folders:
+	runs = [f for f in listdir("results/" + result_folder) if isfile(join("results/"+result_folder, f))]
+	run_avgs = []
+	for run in runs:
+		with open("results/" + result_folder + "/" + run) as f:
+			content = f.readlines()
+		content = [line.strip().split(',') for line in content]
+		times = []
+		for line in content:
+			times += [[float(line[0]),float(line[1])]]
+		during_download = [i[1] for  i in filter(lambda x: x[0] < 30, times)]
+		after_download = [i[1] for  i in filter(lambda x: x[0] >= 30, times)]
+		avg_dur = avg(during_download)
+		avg_aft = avg(after_download)
+		run_avgs += [[avg_dur, avg_aft]]
 
-for file in onlyfiles:
-	print file
-	with open("results/" + file) as f:
-		content = f.readlines()
-	content = [line.strip().split(',') for line in content]
-	times = []
-	for line in content:
-		times += [[float(line[0]),float(line[1])]]
-	during_download = [i[1] for  i in filter(lambda x: x[0] < 60, times)]
-	after_download = [i[1] for  i in filter(lambda x: x[0] >= 60, times)]
-	#print during_download
-	#print after_download
-	avg_dur = avg(during_download)
-	avg_aft = avg(after_download)
-	#print avg_dur
-	#print avg_aft
-	avgs += [[avg_dur, avg_aft]]
+	result_during, result_after = zip(*run_avgs)
+	print "For %s" % result_folder[0:4]
+	print "Average RTT during download was " + str(avg(result_during)) + " : " + str(pstdev(result_during))
+	print "Average RTT after download was " + str(avg(result_after)) + " : " + str(pstdev(result_after))
 
-print avgs
-avg_dur_total, avg_dur_after = zip(*avgs)
-print str(avg(avg_dur_total)) + " " + str(pstdev(avg_dur_total)) 
-print str(avg(avg_dur_after)) + " " + str(pstdev(avg_dur_after))
